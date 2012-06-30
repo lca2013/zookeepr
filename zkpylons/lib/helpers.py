f9692f7e576a944b9b1a22cafe7ff4224efa89dd
James Iseppi <james@iseppi.org>
2012-07-01 02:12:29 +1000

    silly helpers part II

--- zkpylons/lib/helpers.py
+++ zkpylons/lib/helpers.py
@@ -5,6 +5,7 @@ available to Controllers. This module is available to templates as 'h'.
 """
 # Import helpers as desired, or define your own, ie:
 #from webhelpers.html.tags import checkbox, password
+import binascii
 
 from webhelpers.html import escape, HTML, literal, url_escape
 from webhelpers.html.tags import *
